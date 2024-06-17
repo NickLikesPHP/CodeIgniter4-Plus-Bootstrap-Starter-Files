3d23ce0d17ea107cb2bf08a28047f2c31bde2d19
NickLikesPHP <20465973+NickLikesPHP@users.noreply.github.com>
2024-06-17 04:14:17 -0500

    Update top.php
    Simplified code, plus fixed a problem where you could change only the website name.

--- app/Views/partials/bootstrap-basics/top.php
+++ app/Views/partials/bootstrap-basics/top.php
@@ -1,23 +1,13 @@
 <?php
-if(isset($title)){
-    $website_title = website_title(esc($title));
-    if(isset($title_options)){
-        $options = array();
-        if(isset($title_options['website_name'])){$options['WEBSITE_NAME'] = esc($title_options['website_name']);}
-        if(isset($title_options['seperator'])){$options['WEBSITE_NAME_SEPARATOR'] = esc($title_options['seperator']);}
-        if(isset($title_options['website_name_position'])){$options['WEBSITE_NAME_POSITION'] = $title_options['website_name_position'];}
-        $website_title = website_title(esc($title), $options);
-    }
-}else{
-    $website_title = website_title();
-}
+$title = $title ?? null;
+$title_options = $title_options ?? null;
 ?>
 <!doctype html>
 <html lang="en" class="vw-100 min-vh-100" data-bs-theme="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?= $website_title; ?></title>
+        <title><?= esc(website_title($title, $title_options)); ?></title>
         <link rel="icon" type="image/png" href="<?= base_url('favicon.png'); ?>">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
